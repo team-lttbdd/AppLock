521b33a7a4a46aa9f2ea9b73e14233de2ed2e486
unknown <thelinh2711@gmail.com>
2025-03-30 21:46:22 +0700

    complete AppInfo model

--- app/src/main/java/com/example/applock/model/AppInfo.kt
+++ app/src/main/java/com/example/applock/model/AppInfo.kt
@@ -1,3 +1,19 @@
 package com.example.applock.model
 
-data class AppInfo()
+import android.graphics.drawable.Drawable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "appInfo_data_tab")
+data class AppInfo(
+    @ColumnInfo(name = "appInfo_icon")
+    val icon: Drawable,
+    @ColumnInfo(name = "appInfo_name")
+    val name: String,
+    @PrimaryKey
+    @ColumnInfo(name = "appInfo_packageName")
+    val packageName: String,
+    @ColumnInfo(name = "appInfo_isLocked")
+    var isLocked: Boolean
+)
