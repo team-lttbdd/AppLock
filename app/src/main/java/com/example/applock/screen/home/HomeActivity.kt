5333b2676c68e1cbd4e37b9fc91f86cb6e030a4f
DoTruong <dothanhtruong2510@gmail.com>
2025-04-06 13:27:16 +0700

    Giao diện home và các tài nguyên

--- app/src/main/java/com/example/applock/screen/home/HomeActivity.kt
+++ app/src/main/java/com/example/applock/screen/home/HomeActivity.kt
@@ -1,12 +1,13 @@
-package com.example.applock
+package com.example.applock.screen.home
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.applock.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
