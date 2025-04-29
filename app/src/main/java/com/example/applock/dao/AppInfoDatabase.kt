afb22a069bcc5dd0c68acbc676194810086277d5
unknown <thelinh2711@gmail.com>
2025-04-29 17:10:51 +0700

    hoàn thành dựng dao

---
+++ app/src/main/java/com/example/applock/dao/AppInfoDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.applock.dao
+
+import com.example.applock.model.AppInfo
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import android.content.Context
+import androidx.room.TypeConverters
+import com.example.applock.util.Converters
+
+
+@Database(entities = [AppInfo::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class AppInfoDatabase : RoomDatabase() {
+
+    abstract fun appInfoDAO(): AppInfoDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppInfoDatabase? = null
+
+        fun getInstance(context: Context): AppInfoDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppInfoDatabase::class.java,
+                        "appInfo_data_db"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
