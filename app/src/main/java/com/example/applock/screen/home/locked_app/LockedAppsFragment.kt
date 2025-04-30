f29f4aa32f480e003dbf2be65c1ad93b2acea8ec
DoTruong <dothanhtruong2510@gmail.com>
2025-04-30 08:49:18 +0700

    chuyen sang nhanh truong

---
+++ app/src/main/java/com/example/applock/screen/home/locked_app/LockedAppsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.applock.screen.home.locked_app
+
+import androidx.fragment.app.Fragment
+import com.example.applock.R
+
+class LockedAppsFragment : Fragment(R.layout.fragment_locked_apps) {
+    // Nội dung sẽ được triển khai ở bước sau
+}
