3f15f5c77cbece14162d41dabf13e3e93411e2ea
unknown <thelinh2711@gmail.com>
2025-05-18 11:09:30 +0700

    thêm custom PatternLockView và xong SetLockPatternActivity, LockpatternActivity

--- app/src/main/java/com/example/applock/screen/validate_lock_pattern/LockPatternActivity.kt
+++ app/src/main/java/com/example/applock/screen/validate_lock_pattern/LockPatternActivity.kt
@@ -1,21 +1,68 @@
 package com.example.applock.screen.validate_lock_pattern
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.applock.R
+import android.content.Intent
+import android.os.Build
+import android.view.LayoutInflater
+import androidx.annotation.RequiresApi
+import com.example.applock.databinding.ActivityLockPatternBinding
+import com.example.applock.preference.MyPreferences
+import com.example.applock.base.BaseActivity
+import com.example.applock.custom.lock_pattern.PatternLockView
+import com.example.applock.custom.lock_pattern.PatternLockView.PatternViewMode
+import com.example.applock.custom.lock_pattern.listener.PatternLockViewListener
+import com.example.applock.screen.home.HomeActivity
+import com.example.applock.util.AnimationUtil
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
-class LockPatternActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_lock_pattern)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class LockPatternActivity : BaseActivity<ActivityLockPatternBinding>() {
+
+    private lateinit var correctPattern: List<PatternLockView.Dot>
+    private lateinit var tempPattern : ArrayList<PatternLockView.Dot>
+
+    override fun getViewBinding(layoutInflater: LayoutInflater): ActivityLockPatternBinding {
+        return ActivityLockPatternBinding.inflate(layoutInflater)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun initData() {
+        val gson = Gson()
+        val json = MyPreferences.read(MyPreferences.PREF_LOCK_PATTERN, null)
+        val type = object : TypeToken<List<PatternLockView.Dot>>() {}.type
+        correctPattern = gson.fromJson(json, type)
+    }
+
+    override fun setupView() {
+    }
+
+    override fun handleEvent() {
+        binding.patternLockView.addPatternLockListener(object :
+            PatternLockViewListener {
+            override fun onStarted() {
+
+            }
+
+            override fun onProgress(progressPattern: MutableList<PatternLockView.Dot>?) {
+
+            }
+
+            override fun onComplete(pattern: MutableList<PatternLockView.Dot>?) {
+                tempPattern = pattern?.let { ArrayList(it) } ?: arrayListOf()
+
+                if (pattern != correctPattern) {
+                    AnimationUtil.setTextWrong(binding.patternLockView, binding.tvDrawAnUnlockPattern, tempPattern)
+
+                } else {
+                    binding.patternLockView.setPattern(PatternViewMode.CORRECT, tempPattern)
+                    startActivity(Intent(this@LockPatternActivity,  HomeActivity::class.java))
+                    finish()
+                }
+
+            }
+
+            override fun onCleared() {
+
+            }
+        })
     }
 }
